c0f4116c0496a2a3611eec8cc57b275bf028997f
Lorran Rodrigues <lorran.rodrigues@elevaeducacao.com.br>
2021-05-31 20:00:51 -0300

    change api path

--- ui/lib/provider/api_provider.dart
+++ ui/lib/provider/api_provider.dart
@@ -5,7 +5,7 @@ import 'package:taxonomy_method/model/taxonomy_input.dart';
 import 'package:taxonomy_method/model/model_results.dart';
 
 class ApiProvider {
-  final String _baseUrl = "http://localhost:8000";
+  final String _baseUrl = "https://n4c401xtn7.execute-api.us-east-2.amazonaws.com/prod";
 
   final Dio _dio =
       Dio(BaseOptions(headers: {"Access-Control-Allow-Origin": "*"}));
